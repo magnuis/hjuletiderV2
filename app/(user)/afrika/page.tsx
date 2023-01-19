4534cc0cb19c9fa6f1ad3906b6459783d06a53d4
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-01-19 16:03:27 +0100

    initial header

---
+++ app/(user)/afrika/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Ikke laget enda</h1>
+    </div>
+  )
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,21 +3,46 @@
 import Link from 'next/link'
 import { useScrollDirection } from '../hooks/UseScrollDirection'
 import { FaBicycle } from 'react-icons/fa'
+import { usePathname } from 'next/navigation'
 
 export default function Header() {
   const scrollDir = useScrollDirection()
+  const pathname = usePathname()
   return (
     <div
       className={`sticky z-10 ${
         scrollDir === 'down' ? '-top-20' : 'top-0'
-      } h-20transition-all duration-500 flex items-center p-6`}
+      } h-20transition-all duration-500 p-6`}
     >
       <header>
-        <div className="">
+        <div className="flex flex-row justify-between">
           <Link href={'/'}>
             {' '}
             <FaBicycle className="text-gray-900 h-12 w-12" />
           </Link>
+          <div className="flex items-center gap-2">
+            <Link className={`${pathname == '/' ? 'text-yellow-500' : 'text-black'}`} href={'/'}>
+              Home
+            </Link>
+            <Link
+              className={`${pathname == '/norge-pa-langs' ? 'text-yellow-500' : 'text-black'}`}
+              href={'/norge-pa-langs'}
+            >
+              Norge på langs
+            </Link>
+            <Link
+              className={`${pathname == '/afrika' ? 'text-yellow-500' : 'text-black'}`}
+              href={'/afrika'}
+            >
+              Afrika
+            </Link>
+            <Link
+              className={`${pathname == '/mindre-turer' ? 'text-yellow-500' : 'text-black'}`}
+              href={'/mindre-turer'}
+            >
+              Mindre turer
+            </Link>
+          </div>
         </div>
       </header>
     </div>

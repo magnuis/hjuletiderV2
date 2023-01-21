57c1f53dd6aabc3980e451d4503261263b2a62ab
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-01-22 00:32:31 +0100

    feat/bottom navigation

---
+++ components/norge-pa-langs/stagePage/BottomStageNavigation.tsx
@@ -0,0 +1,40 @@
+import ClientSideRoute from '../../ClientSideRoute'
+import { HiOutlineArrowSmLeft, HiOutlineArrowSmRight } from 'react-icons/hi'
+
+interface BottomStageNavigationProps {
+  dayNo: number
+}
+export default function BottomStageNavigation({ dayNo }: BottomStageNavigationProps) {
+  return (
+    <div className="flex flex-row space-x-7 cursor-default">
+      {dayNo == 1 && (
+        <span className="flex flex-row mt-56 space-x-1 items-center text-gray-400">
+          <HiOutlineArrowSmLeft className="h-8 w-8" />
+          <p className="text-lg">Dagen før</p>
+        </span>
+      )}
+      {dayNo > 1 && (
+        <ClientSideRoute route={`norge-pa-langs/dag-${dayNo - 1}`}>
+          <span className="flex flex-row mt-56 space-x-1 items-center">
+            <HiOutlineArrowSmLeft className="h-8 w-8" />
+            <p className="text-lg">Dagen før</p>
+          </span>
+        </ClientSideRoute>
+      )}
+      {dayNo == 25 && (
+        <span className="flex flex-row mt-56 space-x-1 items-center text-gray-400">
+          <p className="text-lg">Dagen etter</p>
+          <HiOutlineArrowSmRight className="h-8 w-8" />
+        </span>
+      )}
+      {dayNo < 25 && (
+        <ClientSideRoute route={`norge-pa-langs/dag-${dayNo + 1}`}>
+          <span className="flex flex-row mt-56 space-x-1 items-center">
+            <p className="text-lg">Dagen etter</p>
+            <HiOutlineArrowSmRight className="h-8 w-8" />
+          </span>
+        </ClientSideRoute>
+      )}
+    </div>
+  )
+}

e8c808c94fb8533b610d6de78fd0bae0c0f5dba4
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-02-24 18:14:11 +0100

    add africa placeholder

--- app/(user)/afrika/page.tsx
+++ app/(user)/afrika/page.tsx
@@ -1,7 +1,47 @@
+import imageUrlBuilder from '@sanity/image-url'
+import { groq } from 'next-sanity'
+import Image from 'next/image'
+import StageCard from '../../../components/StageCard'
+import { client } from '../../../lib/sanity.client'
+import { Stage } from '../../../type'
+import { inter } from '../../../lib/fonts'
+import Link from 'next/link'
+
+const builder = imageUrlBuilder(client)
+
 export default function Page() {
   return (
-    <div>
-      <h1>Ikke laget enda</h1>
+    <div className="max-w-7xl mx-auto flex flex-col space-y-6">
+      <div className="relative h-96 mb-10">
+        <Image
+          priority
+          className="absolute object-center object-cover top-0 h-full w-full opacity-100"
+          src={builder
+            .image(
+              'https://cdn.sanity.io/images/14nn8v5u/production/f400482085e4a94708bc2f23b33ffc77c0cb6c2e-4032x3024.jpg'
+            )
+            .url()}
+          alt={'Rafting på Nilen'}
+          fill
+        />
+      </div>
+      <div className="max-w-7xl mx-10 flex flex-col space-y-8">
+        <hr className="border-black" />
+        <p className="text-5xl font-bold ">Afrika</p>
+        <p>
+          Fra januar 2020 og fram til Covid-19s inntog i Norge reiste Magnus gjennom Afrika. Turen
+          startet i Kenya, med ti dager på et barnehjem. Deretter gikk turen hele veien ned til
+          Sør-Afrika, via Rwanda, Uganda, Tanzania, Malawi, Zambia og Zimbabwe. Denne siden skal gi
+          deg et innblikk i turen - så fort Magnus finner tid til å gjøre det.{' '}
+        </p>
+        <p>
+          Ønsker du å se mer om andre turer kan du gå tilbake til
+          <Link href={'/'}>
+            <span className="ml-1 underline text-blue-700">forsiden</span>
+          </Link>
+        </p>
+      </div>
+      <div className="mt-50"></div>
     </div>
   )
 }

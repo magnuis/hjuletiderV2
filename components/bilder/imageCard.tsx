c50728694cef83ea3c6955087ca039704c373266
Magnus Sagmo <69582539+magnuis@users.noreply.github.com>
2023-02-26 13:44:29 +0100

    display images

---
+++ components/bilder/imageCard.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import imageUrlBuilder from '@sanity/image-url'
+import { client } from '../../lib/sanity.client'
+
+const builder = imageUrlBuilder(client)
+
+interface ImageCardProps {
+  src: string
+  alt: string
+}
+export default function ImageCard({ src, alt }: ImageCardProps) {
+  return (
+    <div className="mx-auto relative h-24 w-full sm:h-44 hover:cursor-pointer opacity-95 hover:opacity-100 transition-transform duration-200 ease-out">
+      <Image
+        src={builder.image(src).url()}
+        alt={alt}
+        className="object-cover object-left lg:object-center"
+        fill
+      />
+    </div>
+  )
+}
